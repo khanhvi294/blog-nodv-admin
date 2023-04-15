e6a6e93a410444f6ecd289e4a7ef86399aecc1f1
Vi Nguyen <khanhvi2941@gmail.com>
2023-04-15 19:01:00 +0700

    fix layout

--- src/layouts/DefaultLayout.js
+++ src/layouts/DefaultLayout.js
@@ -3,11 +3,11 @@ import Navbar from "./components/Navbar";
 import SidebarLeft from "./components/SidebarLeft";
 import { Outlet } from "react-router-dom";
 
-const DefaultLayout = ({ children }) => {
+const DefaultLayout = () => {
   return (
     <>
       <div className="flex bg-slate-100 min-h-screen">
-        <div className="w-16">
+        <div className="w-16 z-[9999]">
           <SidebarLeft />
         </div>
         <div className="flex-1 overflow-hidden">
--- src/layouts/components/SidebarLeft.js
+++ src/layouts/components/SidebarLeft.js
@@ -148,14 +148,16 @@ const SidebarLeft = () => {
       icon: (
         <svg
           xmlns="http://www.w3.org/2000/svg"
+          fill="none"
           viewBox="0 0 24 24"
-          fill="currentColor"
+          strokeWidth={1.5}
+          stroke="currentColor"
           className="w-6 h-6"
         >
           <path
-            fillRule="evenodd"
-            d="M3 2.25a.75.75 0 01.75.75v.54l1.838-.46a9.75 9.75 0 016.725.738l.108.054a8.25 8.25 0 005.58.652l3.109-.732a.75.75 0 01.917.81 47.784 47.784 0 00.005 10.337.75.75 0 01-.574.812l-3.114.733a9.75 9.75 0 01-6.594-.77l-.108-.054a8.25 8.25 0 00-5.69-.625l-2.202.55V21a.75.75 0 01-1.5 0V3A.75.75 0 013 2.25z"
-            clipRule="evenodd"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3 3v1.5M3 21v-6m0 0l2.77-.693a9 9 0 016.208.682l.108.054a9 9 0 006.086.71l3.114-.732a48.524 48.524 0 01-.005-10.499l-3.11.732a9 9 0 01-6.085-.711l-.108-.054a9 9 0 00-6.208-.682L3 4.5M3 15V4.5"
           />
         </svg>
       ),
@@ -174,40 +176,6 @@ const SidebarLeft = () => {
         </svg>
       ),
       path: "/reports",
-    },
-    {
-      title: "Warnings",
-      icon: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
-          />
-        </svg>
-      ),
-      iconActive: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            fillRule="evenodd"
-            d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ),
-      path: "/warnings",
     },
   ];
   return (
